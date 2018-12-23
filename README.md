f807d9735ef2dd3c06440a4105031990da811f12
Daniel Barkley <danielbarkley@daniels-mbp.attlocal.net>
2018-12-22 21:26:16 -0600

    updated READme,Updated Index.HTML, fixed a few bugs in game.js, stil some bugs I need to work out.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Word_Guess_Game
+
+LInk for Portfolio: https://dbbarkley.github.io/Bootstrap-Portfolio/
+
+Link for Word Guess Game: https://dbbarkley.github.io/Word_Guess_Game/
+
+What the project does:
+
+The Word Guess Game selects a random set of words with a Star Trek theme and allows the users to tyr to guess what the word is by typing on their keyboard. The user has 10 guesses before the lose the. 
+
+Why the project is useful:
+
+For myself, this project was very helpful to practice writing and debugging javascript. It's one thing to see and read javascript, but you don't really learn it until you just write it out. This was a frustating at times but I know that I learned a ton! 
+
+Who maintains and contributes to the project:
+
+So as of Saturday 12.22.18 at 8:40 PM I'm looing over my code and I realize that I could have written things out better. 
+Becuase this is for class I'll have to ask to see when I'm allowed to update it.
+
+File paths:
+
+index.html
+assests/css/style.css
+assests/css/reset.css
+
+assets/javascript/game.js
+
+assets/images
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,48 +3,67 @@ var possibleWords = ["voyager", "vulcan", "picard", "spock", "grups", "khan", "p
 var rightLetter = [];
 var wrongLetter = [];
 var underScore = [];
+var newScore = [];
 var randomNumber = [Math.floor(Math.random() * possibleWords.length)];
-var randomWord = possibleWords[randomNumber]
-var wins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var loses = [10, 9, 8, ,7, 6, 5, 4, 3, 2, 1, 0];
-var remainingGuesses = [10, 9, 8, ,7, 6, 5, 4, 3, 2, 1, 0];
-
+var randomWord = possibleWords[randomNumber];
+var wins;
+var loses;
+var lives = 10
+//==============================================================================
+//DOM munipulation
 var generateUnderScore = document.getElementById("word");
 var generateWrongLetter = document.getElementById("wrongletters");
 var winGame = document.getElementById("wins");
+var showLives = document.getElementById("lives");
+//==============================================================================
 //testing
 console.log(randomWord);
-
 // creates the underscores needed for word length
 var newUnderScore = () => {
     for(var i = 0; i < randomWord.length; i++) {
         underScore.push("_ ");  
-    }   console.log(generateUnderScore);
-    generateUnderScore.innerHTML = underScore.join(" ");
+    }generateUnderScore.innerHTML = underScore.join(" ");
+    console.log(generateUnderScore);
     return underScore;
-}
-
-
+};
+//==============================================================================
 // testing
 console.log(newUnderScore());
-
 // capture users guess and logs them into the correct array
+
 document.addEventListener("keypress", (event) => {
     var keyword = String.fromCharCode(event.keyCode);
     if(randomWord.indexOf(keyword) > -1 ) {
         rightLetter.push(keyword);
         underScore[randomWord.indexOf(keyword)] = keyword.toUpperCase();
         generateUnderScore.innerHTML = underScore.join(" ");
+            if(randomWord.length == rightLetter.length) {
+                for(var k = 0; k < wins.length; k++); {
+                    winGame.innerHTML = winGame[k];
+                }
+                alert("you win! You guessed " + randomWord + ".");
+                console.log(winGame);
+                
+            
+            }
            }else(wrongLetter.push(keyword.toUpperCase())); {
             document.getElementById("wrongletters").innerHTML = wrongLetter; 
+
         }
+        console.log(rightLetter);
+        console.log(wrongLetter);
     });
-  
-    /*
-    underScore.join(" ") == randomWord && keyword;
-    for(var k = 0; k > wins.length; k--);
-    winGame.innerHTML = wins[k]; 
-    alert("you win");*/
+
+
+//==============================================================================
+    
+    var display = function () { //print outs for end game
+        showLives.innerHTML = "You have " + lives + " lives";
+    };
+    console.log(display());
+
+    
+    
 
 
 
